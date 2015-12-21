0fb8c9bb4e34963be15d1e8c4b9c5dde5a7cd7b8
Grant Miner <xxgsoftware@gmail.com>
2015-12-20 18:40:29 -0600

    Bump dependencies, beautify code, use promises

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 'use strict';
 
 var program = require('commander');
@@ -6,28 +7,28 @@ var packagejson = require('./package.json');
 var path = require('path');
 
 program
-.version(packagejson.version)
-.option('-l, --listen [port]', 'TCP port to listen on for HTTP', parseInt)
-.option('-s, --smtp [port]', 'TCP port to listen on for SMTP', parseInt)
-.option('-f, --file [file]', 'File to save SQLite database in')
-.parse(process.argv);
+    .version(packagejson.version)
+    .option('-l, --listen [port]', 'TCP port to listen on for HTTP', parseInt)
+    .option('-s, --smtp [port]', 'TCP port to listen on for SMTP', parseInt)
+    .option('-f, --file [file]', 'File to save SQLite database in')
+    .parse(process.argv);
 
 var RestSmtpSink = require('./index');
 
 var server = new RestSmtpSink(program);
-server.on('info', function (info) {
-	console.log('info: ' + info);
+server.on('info', function(info) {
+    console.log('info: ' + info);
 })
 
 server.start();
 
-var interval = 1000; // how often to refresh our measurement 
+var interval = 1000; // how often to refresh our measurement
 
 //var monitor = require('event-loop-monitor');
 
 // data event will fire every 4 seconds
 /*monitor.on('data', function(latency) {
-  console.log(latency); // { p50: 1026, p90: 1059, p95: 1076, p99: 1110, p100: 1260 }   
+  console.log(latency); // { p50: 1026, p90: 1059, p95: 1076, p99: 1110, p100: 1260 }
 });
 
 monitor.resume(); // to start measuring
